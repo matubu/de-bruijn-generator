1eb2a843266d8c94714e92cf501d893c2404bc3d
Mathias <78111467+matubu@users.noreply.github.com>
2021-04-01 17:44:04 +0200

    Update brute-force-code.js

--- exemple/brute-force-code.js
+++ exemple/brute-force-code.js
@@ -2,22 +2,21 @@ const { deBruijn } = require('../src'),
   codeLength = 6;
 
 class CodePadlock  {
-  constructor(codeLength) {
-    this.codeLength = codeLength;
+  constructor() {
     this.code = new Array(codeLength).fill(0).map(v => Math.floor(Math.random() * 10)).join('');
     this.codeInput = '';
     console.log(this.code);
   }
   test(digit) {
     this.codeInput += digit;
-    let overflow = this.codeInput.length - this.codeLength;
+    let overflow = this.codeInput.length - codeLength;
     if (overflow > 0) this.codeInput = this.codeInput.slice(overflow);
     return this.code == this.codeInput;
   }
 }
 
 let generator = deBruijn(10, codeLength),
-  codePadlock = new CodePadlock(codeLength);
+  codePadlock = new CodePadlock();
 
 console.time('Took')
 let v = generator.next().value;
